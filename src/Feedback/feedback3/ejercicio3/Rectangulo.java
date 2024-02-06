970c6846fa6ad0494837173606f3824e67066c8e
Eric Fraguas <102832951+efraguas@users.noreply.github.com>
2024-02-06 22:45:28 +0100

    Feedback 3 de la asignatura de Programación de Servicios y Procesos de 2ºDAM-UAX 2024

--- src/Feedback/feedback3/ejercicio3/Rectangulo.java
+++ src/Feedback/feedback3/ejercicio3/Rectangulo.java
@@ -7,11 +7,19 @@ public class Rectangulo implements Serializable {
     private double perimetro;
 
     //Constructores
+    /**
+     * Constructor vacio de la clase Rectángulo
+     */
     public Rectangulo() {
         this.area = 0.0;
         this.perimetro = 0.0;
     }
 
+    /**
+     * Constructor de la clase Rectángulo
+     * @param area
+     * @param perimetro
+     */
     public Rectangulo(double area, double perimetro) {
         this.area = 0.0;
         this.perimetro = 0.0;
@@ -35,7 +43,10 @@ public class Rectangulo implements Serializable {
 
     //Metodos
 
-
+    /**
+     * Devuelve una cadena formateada con el area y perimetro del rectángulo.
+     * @return  y perimetro del rectángulo.
+     */
     @Override
     public String toString() {
         return String.format("================\nArea: %10.0f\nPerimetro: %5.0f\n================",getArea(),getPerimetro());
